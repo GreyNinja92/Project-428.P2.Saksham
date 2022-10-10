e68dbb663962ca9373f552f0c60caa68b917f816
Saksham Khatod <saksham.khatod92@gmail.com>
2022-10-10 01:56:18 -0500

    Fix Autogenerating objects #1

--- Assets/RamenSpawner.cs
+++ Assets/RamenSpawner.cs
@@ -9,33 +9,32 @@ public class RamenSpawner : MonoBehaviour
     public Vector3 origin = Vector3.zero;
     public float radius = 10;
     public Vector3 lastPosition = Vector3.zero;
-    public Transform myTransform;
     public bool isMoving = false;
 
     void Start()
     {
+        Debug.Log("Starting a new object");
         isMoving = false;
         StartCoroutine(registerPosition());
-        InvokeRepeating("spawnRamen", 4.0f, 3.0f);
+        InvokeRepeating("spawnRamen", 2.0f, 3.0f);
     }
 
     IEnumerator registerPosition()
     {
-        yield return new WaitForSeconds(2);
-        myTransform = ramen.transform;
-        lastPosition = myTransform.position;
-        Debug.Log("Initial Position " + lastPosition);
+        yield return new WaitForSeconds(1);
+        lastPosition = ramen.transform.position;
+        Debug.Log("Setting Initial Position " + lastPosition);
     }
 
     void spawnRamen() {
         Debug.Log("Current Position " + ramen.transform.position);
-        if ( ramen.transform.position != lastPosition ) {
+        if (ramen.transform.position != lastPosition) {
             isMoving = true;
             Debug.Log("Old pos " + ramen.transform.position);
             GameObject newObject = Instantiate(ramen, lastPosition, Quaternion.identity);
-            lastPosition = newObject.transform.position;
             Debug.Log("Instantiated new object");
             Debug.Log(newObject.transform.position);
+            Destroy(GetComponent<RamenSpawner>());
         } else {
             isMoving = false;
         }
